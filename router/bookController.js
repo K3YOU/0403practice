96f194194be4484213fcc80c4d75bd4d3804445a
K3YOU <7592667@naver.com>
2023-01-20 17:52:16 +0900

    0120

--- router/bookController.js
+++ router/bookController.js
@@ -20,7 +20,21 @@ function sortBookByBookSold(bookDB){
 
 // 번호 순으로 오름차순 정렬하기 (전체보기)
 function sortBookByBookNo(bookDB){
+    for(i=0;i<bookDB.length;i++){
+        var minNo = bookDB[i]["bookNo"]
+        var minIndex = i
 
+        for(j= i;j<bookDB.length;j++){
+            if(minNo>bookDB[j]["bookNo"]){
+                minNo = bookDB[j]["bookNo"]
+                minIndex = j
+            }
+        
+        var temp = bookDB[i]
+        bookDB[i] = bookDB[minIndex]
+        bookDB[minIndex] = temp;
+        }
+    }
 }
 
 
@@ -48,14 +62,18 @@ module.exports = function(app){
     app.get("/bookAllList",function(req,res){
         //필요한 변수 지정 --> 저 페이지에 있어야할 것들이 무엇인가?
         var log = req.session.log     // 로그인을 한 경우 로그인이 되었다고 알려줘야하므로
-        var bookDB = req.session.log
+        var bookDB = req.session.bookDB
         var name = req.session.name
        
         //오름차순 정렬함수(bookDB)
         sortBookByBookNo(bookDB)
 
         //렌더링에 필요한 변수들 지정
-
+        var renderData= {
+            'log' : log,
+            'bookDB' : bookDB,
+            'name' :name
+        }
 
         res.render("book/bookAllList.ejs",renderData)
     });
